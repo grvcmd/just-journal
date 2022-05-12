d0abaeb63c7a0e9a196de664fdbb2e15f3cab90e
Ken Valverde <kenvalverde@Kens-MacBook-Air.local>
2022-05-12 18:58:51 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const axios = require('axios');
+app.use(express.json()); // Allows us to access req body as req.body
+
+// Connect to MongoDB
+mongoose
+    .connect(process.env.MONGODB_CONNECTION_URL)
+    .then(() => {
+        console.log('Database connection success!')
+    })
+    .catch((err) => {
+        console.error("MongoDB connection error", err)
+    });
+const port = process.env.PORT
+app.use(cors());
+
+// Routes for each model
+const topicRouter = require('./routes/topics')
+const entryRouter = require('./routes/entries')
+
+app.use('/topics', topicRouter);
+app.use('/entries', entryRouter);
+
+// Endpoint for the homepage
+app.get('/', (req, res) => {
+    res.send('This is the homepage')
+});
+
+
+// Server will start listening and log a message if OK
+app.listen(port, () => {
+    console.log("Server started listening on port " + port + ".");
+  });
+
+// Error handler
+app.use(function (err, req, res, next) {
+    console.error(err.message);
+    if (!err.statusCode) 
+        err.statusCode = 500;
+    res.status(err.statusCode).send(err.message);
+});
