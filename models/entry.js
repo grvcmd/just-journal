95d7157a09f3a0867816dd700b1313cc3bae2a7a
Ken Valverde <kenvalverde@Kens-MacBook-Air.local>
2022-05-17 15:30:16 -0500

    Create entryName attribute

--- models/entry.js
+++ models/entry.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const entrySchema = new Schema({
+    entryName: {
+        type: String,
+        required: true
+    },
     topic: String,
     text: String,
     date_added: {
